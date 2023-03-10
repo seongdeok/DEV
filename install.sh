63bbf724e88cf98eb13cf22170439047aac42291
seongdeok <33082104+seongdeok@users.noreply.github.com>
2023-03-10 11:24:11 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 sudo apt update
-sudo apt upgrade
-sudo apt install -y cmake terminator git build-essential cscope ctags vim curl zsh fonts-powerline
+sudo apt upgrade -y
+sudo apt install -y cmake terminator git build-essential cscope universal-ctags vim curl zsh fonts-powerline
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/.vimrc
 sudo apt-get -y install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+cd /tmp
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions
+source ./zsh-autosuggestions/zsh-autosuggestions.zsh
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
