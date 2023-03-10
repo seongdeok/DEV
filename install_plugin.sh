2cdda693edcfff30077e8dc162de9d10bfd7926b
seongdeok <33082104+seongdeok@users.noreply.github.com>
2023-03-10 11:28:12 +0900

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/zsh
 cd /tmp
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
