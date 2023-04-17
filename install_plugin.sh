49c7c8238746469c965fd399b3c7216084f60dd8
seongdeok <33082104+seongdeok@users.noreply.github.com>
2023-04-17 14:35:01 +0900

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/zsh
-cd /tmp
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
-#source ./zsh-autosuggestions/zsh-autosuggestions.zsh
-echo "Add zsh-autosuggestions to your .zshrc"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
+echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
