9167ba8b3dfdf04c1a70b71afe7abeddf4de3897
seongdeok <33082104+seongdeok@users.noreply.github.com>
2023-04-17 15:34:45 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 if [ -f "/home/worker/.bee" ]; then
-  TARGET = "brew"
+  TARGET="brew"
   echo "Install DEV Env with LinuxBrew"
 else
-  TARGET = "native"
+  TARGET="native"
   echo "Install DEV Env on Linux"
 fi
 ./install_package.sh $TARGET
