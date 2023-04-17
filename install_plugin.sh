82c0b641b70600febc1505659c62a039ed685567
seongdeok <33082104+seongdeok@users.noreply.github.com>
2023-04-17 19:39:31 +0900

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/usr/bin/sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
