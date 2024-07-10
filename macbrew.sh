3a9d7b386b6ce4c0514c61366b9cf17a5822d366
seongdeok <33082104+seongdeok@users.noreply.github.com>
2024-07-10 15:22:15 +0900

    Add files via upload

---
+++ macbrew.sh
@@ -0,0 +1,8 @@
+brew install iterm2 —cask
+brew install vim 
+brew install —cask craft rectangle raycast appcleaner
+
+brew install --cask karabiner-elements
+brew install --cask fig
+brew install --cask massy
+brew install --cask alt-tab
