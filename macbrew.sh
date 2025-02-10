31ed40af0abe3c133d72f7bd6aaa9853add78e6a
seongdeok <33082104+seongdeok@users.noreply.github.com>
2025-02-10 14:09:04 +0900

    Update macbrew.sh

--- macbrew.sh
+++ macbrew.sh
@@ -1,8 +1,8 @@
 brew install iterm2 —cask
 brew install vim 
-brew install —cask craft rectangle raycast appcleaner
-
+brew install —-cask craft rectangle raycast appcleaner
+brew install --cask keka
 brew install --cask karabiner-elements
-brew install --cask fig
-brew install --cask massy
+brew install --cask maccy
 brew install --cask alt-tab
+brew install --cask macs-fan-control
