314a594db0c76448232b8b376ce7ecce910ebcea
seongdeok <33082104+seongdeok@users.noreply.github.com>
2025-02-17 10:45:17 +0900

    Update macbrew.sh

--- macbrew.sh
+++ macbrew.sh
@@ -3,8 +3,7 @@ brew install git cscope universal-ctags vim curl zsh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/.vimrc
 vim +PluginInstall +qall
-brew install iterm2 —cask
-brew install vim 
+brew install --cask iterm2
 brew install —-cask craft rectangle raycast appcleaner
 brew install --cask keka
 brew install --cask karabiner-elements
