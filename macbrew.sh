f4ed26e14ee50579694dba900361460f2af5e032
duk0668 <duk0668@duk0668s-Mac-Pro.local>
2025-03-12 20:11:13 +0900

    add things

--- macbrew.sh
+++ macbrew.sh
@@ -1,7 +1,8 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
-brew install git cscope universal-ctags vim curl zsh
+brew install git cscope universal-ctags vim curl zsh neovim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./.vimrc ~/.vimrc
+ln -s .vimrc ~/.vimrc
 vim +PluginInstall +qall
 brew install --cask ghostty
 brew install —-cask craft rectangle raycast appcleaner
@@ -10,5 +11,35 @@ brew install --cask karabiner-elements
 brew install --cask maccy
 brew install --cask alt-tab
 brew install --cask macs-fan-control
-brew install --cask craft
 brew install --cask visual-studio-code
+brew install --cask font-meslo-for-powerlevel10k
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
+echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+ln -s ghostty ~/.config/ghostty
+
+# For VSCode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# For VSCode Insiders
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+
+# For VSCodium
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
+
+# To enable global key-repeat
+# this is helpful if you're using Vim in a PWA like code-server
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+ln -s ./borders ~/.config/borders
