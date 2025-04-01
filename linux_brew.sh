bcd0379b50e170408fb8ac18f0003a3c6e0cb589
seongdeok <33082104+seongdeok@users.noreply.github.com>
2025-04-01 18:35:04 +0900

    Update linux_brew.sh

--- linux_brew.sh
+++ linux_brew.sh
@@ -1,15 +1,15 @@
 brew install starship nvim zoxide curl bat ctags cscope fzf tmux yazi eza npm
 echo "alias vi='nvim'" >> ~/.bashrc
-git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1
-cd nerd-fonts
-./install.sh
-cd ~
-eval "$(starship init bash)" >> ~/.bashrc
+
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-mkdir ~/.config
+mkdir -p ~/.config
 ln -s ./nvim ~/.config/nvim
 
 echo "alias ls='eza --color=always --group-directories-first --icons'" >> ~/.bashrc
 echo "alias ll='eza -la --icons --octal-permissions --group-directories-first'" >> ~/.bashrc
 echo "alias l='eza -bGF --header --git --color=always --group-directories-first --icons'" >> ~/.bashrc
+
+ln -s $PWD/nvim ~/.config/nvim
+ln -s $PWD/.tmux.conf ~/.tmux.conf
 
