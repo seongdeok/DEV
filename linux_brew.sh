d305fb4598c12a62cf01480cb2c80118232f0909
seongdeok <33082104+seongdeok@users.noreply.github.com>
2025-04-01 14:09:09 +0900

    Update linux_brew.sh

--- linux_brew.sh
+++ linux_brew.sh
@@ -1,2 +1,7 @@
 brew install starship nvim zoxide curl bat ctags cscope fzf tmux yazi
 echo "alias vi='nvim'" >> ~/.bashrc
+git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1
+cd nerd-fonts
+./install.sh
+cd ~
+eval "$(starship init bash)" >> ~/.bashrc
