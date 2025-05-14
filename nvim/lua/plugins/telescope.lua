1efaea896cbcf1ddb90601463e5fa3d6f3a763f1
seongdeok <33082104+seongdeok@users.noreply.github.com>
2025-05-14 22:30:40 +0900

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,30 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
 			local builtin = require("telescope.builtin")
+
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-h>"] = actions.cycle_history_prev,
+							["<C-l>"] = actions.cycle_history_next,
+						},
+						n = {
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-h>"] = actions.cycle_history_prev,
+							["<C-l>"] = actions.cycle_history_next,
+						},
+					},
+				},
+			})
 			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
