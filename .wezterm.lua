3222d58686fa380b5e1f5828fdbd55fd0edfb131
duk0669 <duk0669@gmail.com>
2025-05-23 22:31:21 +0900

    devshell drop4

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ return {
 	color_scheme = "Catppuccin Mocha",
 	enable_tab_bar = false,
 	font_size = 16.0,
-	font = wezterm.font("MesloLGS NF"),
+	font = wezterm.font("Hack Nerd Font Mono Regular"),
 	-- macos_window_background_blur = 40,
 	--macos_window_background_blur = 30,
 	native_macos_fullscreen_mode = false,
