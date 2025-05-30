a5ddeeed5ea6d108801942faa32672121eda5cd8
seongdeok <33082104+seongdeok@users.noreply.github.com>
2025-05-30 10:21:26 +0900

    Update install.sh

--- devshell/install.sh
+++ devshell/install.sh
@@ -1,3 +1,5 @@
 curl -L https://github.com/DavHau/nix-portable/releases/latest/download/nix-portable-$(uname -m) > ./nix
 chmod +x ./nix
 ln -s ${PWD}/../nvim ${PWD}/.config-devshell/nvim
+mkdir -p ${PWD}/.config-devshell/tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ${PWD}/.config-devshell/tmux/plugins/tpm
