b629e3c79b266f3dc4dfae7d19f5b44e93221720
seongdeok <33082104+seongdeok@users.noreply.github.com>
2025-06-26 23:07:58 +0900

    Update arch_postinstall.sh

--- arch_postinstall.sh
+++ arch_postinstall.sh
@@ -1,8 +1,9 @@
 sudo pacman -S adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts
 sudo pacman -S cscope universal-ctags curl ttf-font-awesome ghostty bluez bluez-utils network-manager-applet
 yay -S kime-bin ttf-nerd-fonts-symbols-mono tmux neovim ripgrep waynergy google-chrome ttf-nanum icaclient ripgrep teams-for-linux-bin ghostty visual-studio-code-bin
-yay -S appimagelauncher-bin
+
 yay -S zoxide yazi fd bat eza ripgrep ueberzugpp btop duf dust procs tldr nodejs npm zip unzip
+yay -S wezterm-git
 mkdir -p ~/.config/tmux
 mkdir -p ~/.config/.tmux
 # nvim
