8cdd1a96e8c1c5f3370192bedbb6fdad24e1976d
duk0668 <duk0668@duk0668s-Mac-Pro.local>
2025-07-24 11:04:54 +0900

    sync 3

--- end4_arch_postinstall.sh
+++ end4_arch_postinstall.sh
@@ -1,8 +1,8 @@
 sudo pacman -S adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts
 sudo pacman -S cscope universal-ctags curl ttf-font-awesome ghostty bluez bluez-utils network-manager-applet
 yay -S --needed --noconfirm ttf-hack-nerd kime-bin ttf-nerd-fonts-symbols-mono tmux neovim ripgrep waynergy google-chrome ttf-nanum icaclient ripgrep teams-for-linux-bin ghostty visual-studio-code-bin
-yay -S --needed --noconfirm zoxide yazi fd bat eza ripgrep ueberzugpp btop duf dust procs tldr nodejs npm zip unzip
-yay -S wezterm-git
+yay -S --needed --noconfirm zoxide yazi fd bat eza ripgrep ueberzugpp btop duf dust procs tldr nodejs npm zip unzip nwg-displays
+yay -S --needed --noconfirm  wezterm-git
 #yay -S sddm blueman network-manager-applet
 sudo pacman -S networkmanager nm-connection-editor bluez bluez-utils polkit-gnome
 sudo systemctl enable --now NetworkManager
