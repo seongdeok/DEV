5b9605b9384ed11d117b9c9b122bd62bb8a17903
duk0669 <duk0669@gmail.com>
2025-08-26 11:22:54 +0900

    sync

--- macbrew.sh
+++ macbrew.sh
@@ -1,50 +1,84 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap FelixKratz/formulae
+if ! command -v brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 brew update
-brew install git cscope universal-ctags vim curl zsh neovim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s .vimrc ~/.vimrc
+
+# Homebrew formulae
+for pkg in git cscope universal-ctags vim curl zsh neovim ripgrep bash wezterm neofetch borders sketchybar font-hack-nerd-font jq font-sf-pro; do
+  if ! brew list --formula | grep -q "^$pkg$"; then
+    brew install "$pkg"
+  fi
+done
+
+# Homebrew casks
+for cask in ghostty craft rectangle raycast appcleaner keka karabiner-elements maccy alt-tab macs-fan-control visual-studio-code font-meslo-for-powerlevel10k font-jetbrains-mono-nerd-font nikitabobko/tap/aerospace sf-symbols font-sketchybar-app-font; do
+  if ! brew list --cask | grep -q "^$cask$"; then
+    brew install --cask "$cask"
+  fi
+done
+
+# VundleVim
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+# .vimrc symlink
+if [ ! -L "$HOME/.vimrc" ]; then
+  ln -s .vimrc ~/.vimrc
+fi
 vim +PluginInstall +qall
-brew install --cask ghostty craft rectangle raycast appcleaner keka karabiner-elements maccy alt-tab macs-fan-control visual-studio-code font-meslo-for-powerlevel10k font-jetbrains-mono-nerd-font nikitabobko/tap/aerospace
-brew install ripgrep bash wezterm
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
-echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+# Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+# zsh-autosuggestions
+if [ ! -d "$HOME/.zsh/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+fi
+if ! grep -q "zsh-autosuggestions.zsh" ~/.zshrc; then
+  echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+fi
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+# zsh-syntax-highlighting
+if [ ! -d "$HOME/.zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
+fi
+if ! grep -q "zsh-syntax-highlighting.zsh" ~/.zshrc; then
+  echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+fi
 
-ln -s ghostty ~/.config/ghostty
+# fzf
+if [ ! -d "$HOME/.fzf" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+fi
 
-# For VSCode
+# powerlevel10k
+if [ ! -d "$HOME/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+fi
+if ! grep -q "powerlevel10k.zsh-theme" ~/.zshrc; then
+  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+fi
+
+# ghostty symlink
+if [ ! -L "$HOME/.config/ghostty" ]; then
+  ln -s ghostty ~/.config/ghostty
+fi
+
+# VSCode settings
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-
-# For VSCode Insiders
 defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
-
-# For VSCodium
 defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
-
-# To enable global key-repeat
-# this is helpful if you're using Vim in a PWA like code-server
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-
 brew tap FelixKratz/formulae
-brew install borders
-ln -s ./borders ~/.config/borders
+
+# borders symlink
+if [ ! -L "$HOME/.config/borders" ]; then
+  ln -s ./borders ~/.config/borders
+fi
 brew services start borders
-
-brew install neofetch
-
-brew install sketchybar
-brew install font-hack-nerd-font jq
-brew install font-sf-pro
-brew install --cask sf-symbols
-brew install --cask font-sketchybar-app-font
 
