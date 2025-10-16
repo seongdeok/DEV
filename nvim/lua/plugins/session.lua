49a4a2951a845ecf0db3ae675cdbebe7579e65ae
duk0669 <duk0669@gmail.com>
2025-10-16 11:10:47 +0900

    sync

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -3,8 +3,8 @@ return {
 	config = function()
 		require("auto-session").setup({
 			auto_session_enabled = true,
-			auto_save_enabled = true,
-			auto_restore_enabled = true,
+			auto_save_enabled = false,
+			auto_restore_enabled = false,
 			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
 			session_lens = {
 				buftypes_to_ignore = {},
