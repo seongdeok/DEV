2cd2300f03dd1cb14e96047856690b6df59da31c
duk0669 <duk0669@gmail.com>
2025-11-18 10:39:34 +0900

    sync

--- omarchy_hypr/scripts/chatgpt_dropdown.sh
+++ omarchy_hypr/scripts/chatgpt_dropdown.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-CLASS="chrome-app.eaip.lge.com__lgenie_-Default"
-TITLE="LGenie.AI"
+check_http() {
+    local url=$1
+    # -s: 조용히 실행, -o /dev/null: 출력 버림, -w "%{http_code}": HTTP 상태 코드만 출력
+    local status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+    if [[ "$status_code" -ge 200 && "$status_code" -lt 400 ]]; then
+        #echo "✅ $url 접속 성공 (HTTP $status_code)"
+        return 0
+    else
+        #echo "❌ $url 접속 실패 (HTTP $status_code)"
+        return 1
+    fi
+}
+
+if check_http "https://app.eaip.lge.com/lgenie"; then
+  CLASS="chrome-app.eaip.lge.com__lgenie_-Default"
+  TITLE="LGenie.AI"
+  URL="https://app.eaip.lge.com/lgenie/"
+else
+  CLASS="chrome-chatgpt.com__-Default"
+  TITLE="ChatGPT"
+  URL="https://chatgpt.com"
+fi
+
 #현재 활성 창이 dropterm인지 확인
 ACTIVE_DROPTERM=$(hyprctl activewindow -j | jq -r --arg TITLE "$TITLE" 'select(.title == $TITLE) | .address')
 #dropterm 창 전체 중 하나 가져오기
@@ -14,6 +36,8 @@ echo existing=$EXISTING_DROPTERM >> /tmp/msg
 echo window_id=$WINDOW_ID >> /tmp/msg
 echo ws=$CUR_WS >> /tmp/msg
 
+
+
 if [[ -n "$ACTIVE_DROPTERM" ]]; then
 #현재 포커스가 dropterm이면 숨김
   hyprctl dispatch togglespecialworkspace chatgpt 
@@ -25,5 +49,9 @@ elif [[ -n "$WINDOW_ID" ]]; then
 else
 #창이 없으면 새로 실행하고 focus
   #google-chrome-stable "https://app.eaip.lge.com/lgenie/public" --class $CLASS 
-  omarchy-launch-webapp "https://app.eaip.lge.com/lgenie/" 
+  if check_http "https://app.eaip.lge.com/lgenie"; then
+    omarchy-launch-webapp "https://app.eaip.lge.com/lgenie/" 
+  else
+    omarchy-launch-webapp "https://chatgpt.com" 
+  fi
 fi
